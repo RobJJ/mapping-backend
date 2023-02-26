76197dd4c4da1b1e123225979324d1cfa0e750b2
RobJ <robjjohnsenwork@gmail.com>
2023-02-26 13:17:25 +0700

    Initial commit - server, app setup

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const morgan = require("morgan");
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("combined"));
+app.use(cors());
+
+// app.use(express.static(path.join(__dirname, "..", "public")));
+
+// removed this for testing.... - lets you then reroute to the Apollo graphiql ui
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+// });
+
+module.exports = app;
---
+++ server/src/server.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+const path = require("path");
+const colors = require("colors");
+//
+// import { readFile } from "fs/promises";
+const { readFile } = require("fs/promises");
+const { loadFilesSync } = require("@graphql-tools/load-files");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const { mongoConnect } = require("./services/mongo");
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const PORT = process.env.PORT;
+
+const resolversArray = loadFilesSync(path.join(__dirname, "**/*.resolvers.js"));
+// const typesArray = loadFilesSync("**/*", {
+//   extensions: ["graphql"],
+// });
+const typesArray = await readFile("./schema.graphql", "utf-8");
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+async function startApolloServer() {
+  const app = require("./app");
+
+  const schema = makeExecutableSchema({
+    typeDefs: typesArray,
+    resolvers: resolversArray,
+  });
+
+  const server = new ApolloServer({
+    schema: schema,
+  });
+
+  // still going to connect to mongo here for DB
+  await mongoConnect();
+
+  await server.start();
+  // new apollo version 4
+  // tell express to forward any requests at this path to apolloServer!
+  app.use("/graphql", expressMiddleware(server));
+  // old apollo way V3 ---> connect the middleware with express server
+  //   server.applyMiddleware({
+  //     app: app,
+  //     path: "/graphql",
+  //   });
+
+  app.listen(PORT, () => {
+    console.log(`Running graphQL apollo server...`.cyan);
+  });
+}
+
+startApolloServer();
