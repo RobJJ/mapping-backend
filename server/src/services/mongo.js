ef96ffe12d70aa19140a2b3050ecb285bfef17e4
RobJ <robjjohnsenwork@gmail.com>
2023-03-03 10:30:02 +0700

    added logging flow to mongoose

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -12,7 +12,18 @@ mongoose.connection.on("error", (err) => {
   console.log("Error: ", err);
 });
 //
+
+//
 async function mongoConnect(params) {
+  // Could enable logging features here on setup of the function
+  // mongoose.set("debug", true);
+  // var options = {
+  //   logger: console.log,
+  //   loggerLevel: "info",
+  //   maxPoolSize: 10,
+  // };
+  // edit this to pass in options
+  // await mongoose.connect(MONGO_URL, options);
   await mongoose.connect(MONGO_URL);
 }
 async function mongoDisconnect(params) {
