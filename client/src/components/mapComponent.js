0a354f17a9f4e93e5f27292c764d336de33a55de
RobJ <robjjohnsenwork@gmail.com>
2023-03-10 11:24:06 +0700

    got map pulling through

--- client/src/components/mapComponent.js
+++ client/src/components/mapComponent.js
@@ -1,16 +1,27 @@
+import { useEffect, useState } from "react";
 import { GeoJSON } from "react-leaflet";
+import { httpGetGeoJsonData } from "../geojson/requests";
 
 // import useGeoJson from "../geojson/useGeoJson";
 
 export const MapComponent = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    httpGetGeoJsonData().then((res) => setData(res));
+  }, []);
+
+  console.log(data);
+
+  if (!data) return <div>Hey , we loading the data bitch!</div>;
+
   return (
     <>
       <GeoJSON
-      // onEachFeature={onEachFeature}
-      // style={style}
-      // data={data}
-      // data={districtGeoJSON}
-      // ref={testRef}
+        // onEachFeature={onEachFeature}
+        // style={style}
+        data={data}
+        // data={districtGeoJSON}
+        // ref={testRef}
       ></GeoJSON>
     </>
   );
