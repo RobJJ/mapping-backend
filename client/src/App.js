5fa55a8ea96b19e30358403bfd8d8aa7cc89d4cb
RobJ <robjjohnsenwork@gmail.com>
2023-03-12 19:33:59 +0700

    moved both calls to same component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
 import { ApolloProvider } from "@apollo/client";
-import useGeoJson from "./geojson/useGeoJson";
+
 import { client } from "./graphql/graphql.queries";
 import { MainPage } from "./pages/mainPage";
 import { MapPage } from "./pages/mapPage";
 
 function App() {
-  const { geoJsonData } = useGeoJson();
   // console.log(geoJsonData);
 
   return (
     <ApolloProvider client={client}>
       <div className=" w-screen h-screen p-5">
-        {!geoJsonData && <div>Im the loading page...</div>}
-        {geoJsonData && <MapPage geoData={geoJsonData} />}
+        <MapPage />
       </div>
     </ApolloProvider>
   );
--- client/src/pages/mapPage.js
+++ client/src/pages/mapPage.js
@@ -3,10 +3,13 @@ import { MapContainer, TileLayer } from "react-leaflet";
 import MapComponent from "../components/mapComponent";
 import "leaflet/dist/leaflet.css";
 import { httpGetMapboxUrl } from "../mapbox/requests";
+import useGeoJson from "../geojson/useGeoJson";
 
-export const MapPage = ({ geoData }) => {
+export const MapPage = () => {
   const [url, setUrl] = useState(null);
   const mapRef = useRef();
+
+  const { geoJsonData } = useGeoJson();
 
   useEffect(() => {
     (async function runOnce() {
@@ -18,7 +21,7 @@ export const MapPage = ({ geoData }) => {
   console.log("the url bruv", url);
   const defaultPosition = [16.06, 108.21];
 
-  if (!url) return <div>Loading data...</div>;
+  if (!url || !geoJsonData) return <div>Loading data...</div>;
 
   return (
     <MapContainer
@@ -33,7 +36,7 @@ export const MapPage = ({ geoData }) => {
         url={url.MAPBOX_URL}
         // url="https://api.mapbox.com/styles/v1/robjj/clce68ufg003614nvvjlep2ke/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoicm9iamoiLCJhIjoiY2xjZTVva3NhMGQydzN3bGx5cnEwd2I1eSJ9.X-ErXEB4RZMQmGZvsaMGNA"
       />
-      <MapComponent geoData={geoData} />
+      <MapComponent geoData={geoJsonData} />
     </MapContainer>
   );
 };
